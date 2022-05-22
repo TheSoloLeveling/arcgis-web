ab9b0e8ea7dd663cb0ce0c1c59507ca2ed806dc1
bouziane yassine <62574566+TheSoloLeveling@users.noreply.github.com>
2022-05-22 12:58:23 +0100

    django build

---
+++ arcgis_F/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components'
+import SideBar from './components/Sidebar'
+import Dashboard from './components/Dashboard'
+import Map from './Map';
+
+function App() {
+  return (
+    <Div>
+      dqsdsqd
+        <SideBar/>
+        <Dashboard/>
+    </Div>
+  );
+}
+
+export default App;
+const Div = styled.div `
+position: relative;
+`;

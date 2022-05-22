e91f307b0cbe033ecd111e63a032b500d497cd7c
bouziane yassine <62574566+TheSoloLeveling@users.noreply.github.com>
2022-05-22 09:44:39 +0100

    sample

---
+++ arcgis/src/Map.js
@@ -0,0 +1,45 @@
+import React ,{useRef, useEffect} from 'react'
+import {loadModules} from 'esri-loader'
+
+function Map() {
+
+    const MapEl = useRef(null)
+
+    useEffect( () => {
+        let view;
+
+    loadModules(["esri/views/MapView", "esri/Map"], {
+        css:true
+    }).then(([MapView,Map]) => {
+
+        //esriConfig.apiKey = "YOUR_API_KEY";
+
+        const map = new Map({
+            basemap: "arcgis-topographic" // Basemap layer service
+          });
+  
+          const view = new MapView({
+            map: map,
+            center: [-118.805, 34.027], // Longitude, latitude
+            zoom: 13, // Zoom level
+            container: MapEl // Div element
+          });
+    }) 
+    
+    
+    return() => {
+        if (!!view) {
+            view.destroy()
+            view = null
+        }
+    }
+    })
+
+    return (
+        <div style={{height:800}} ref={MapEl}>
+
+        </div>
+    )
+}
+
+export default Map
